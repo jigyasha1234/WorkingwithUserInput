c44f1018df2def0fa03e49da4ec0194e5c89d4c5
Jigyasha Rani <jigrani0000@gmail.com>
2021-12-29 13:55:13 +0530

    assignment-2 done

---
+++ assignments/assignment_2/index.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+
+fs.writeFileSync(
+  "./WORKWITHUSERINPUT\WorkingwithUserInput\assignments\assignment_2/index.html",
+  "<h1>Hello World</h1>",
+  (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log("The written has the following contents:");
+      console.log(fs.readFileSync("books.txt", "utf8"));
+    }
+  }
+);
+const http = require("http");
+const newFile = fs.readFileSync(
+  "./WORKWITHUSERINPUT\WorkingwithUserInput\assignments\assignment_2/index.html",
+  (err, data) => {
+    if (err) console.log(err);
+    console.log(data);
+  }
+);
+var server = http.createServer((req, res) => {
+  if (req.url == "./") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(newFile);
+    res.end();
+  }
+});
+server.listen(3000, "localhost");
